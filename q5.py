797336bf485f96e9f9399be9502c4cd98aac5467
Divya93900 <divyareddy2743@gmail.com>
2024-04-17 14:25:36 +0530

    assignment

--- q5.py
+++ q5.py
@@ -1,14 +1,14 @@
 a=int(input("enter the marks:"))
 b=int(input("enter the marks:"))
 c=int(input("enter the marks:"))
-print((a+b+c)/3)
-if(90>=100):
+avg=((a+b+c)/3)
+if(90<avg<100):
     print("grade A")
-elif(80<=89 ):
+elif(80<avg<89 ):
     print("grade b")
-elif(70<=79 ):
+elif(70<avg<79 ):
     print("grade c")
-elif(60<=69):
+elif(60<=avg<69):
     print("grade d")    
 else:
     print("fail")
